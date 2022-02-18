381f94e6f36b92d579cabccf4d98af31d811217c
sasa1978 <Maximus.78@mail.ru>
2022-02-18 17:26:37 +0300

    Edit devide

--- task1/Calculator.java
+++ task1/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : Integer.MAX_VALUE;
+    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : x >= 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;

2ef1a2e8256aee695f1691922fb9d54bb2ebd70a
sasa1978 <Maximus.78@mail.ru>
2022-02-18 16:26:43 +0300

    Initial commit

---
+++ task1/Main1.java
@@ -0,0 +1,19 @@
+public class Main1 {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+        // Складываем 1 + 2 = 0
+        int a = calc.plus.apply(1, 2);
+        // Вычитаем 1 - 1 = 0
+        int b = calc.minus.apply(1,1);
+        // Попытка поделить результат первого сложения на результат второго вычитания
+        // Дели 3 на 0 и программа падает
+        int c = calc.devide.apply(a, b);
+
+        // Сюда уже выполнение не доходит
+        calc.println.accept(c);
+
+        // Для решения задачи используем тернарный оператор в делении.
+        // Если деление на 0, то возвращаем максимально возможный результат.
+    }
+}
